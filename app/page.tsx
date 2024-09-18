f75cf098ceace895c65c160eb1e6cd8b6c0cf756
Алексей <savageseed@yandex.ru>
2024-09-18 14:54:17 +0500

    Products group

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,6 @@
 import { Container, Title, TopBar, Filter } from "@/components/shared";
 import { ProductCard } from "@/components/shared/product-card";
+import { ProductGroupList } from "@/components/shared/products-group-list";
 
 export default function Home() {
   return (
@@ -20,12 +21,57 @@ export default function Home() {
           {/* Product List */}
           <div className="flex-1">
             <div className="flex flex-col gap-16">
-              <ProductCard
-                id={0}
-                name={"Сырная"}
-                price={420}
-                imageUrl="https://media.dodostatic.net/image/r:584x584/11EE7D610D2925109AB2E1C92CC5383C.avif"
-              />
+              <ProductGroupList title={"Пиццы"} items={[{
+                id: 1,
+                name: 'Сырная',
+                imageUrl: 'https://media.dodostatic.net/image/r:584x584/11EE7D610D2925109AB2E1C92CC5383C.avif',
+                price: 550,
+                items: [{price: 550}],
+                
+              },
+              {
+                id: 2,
+                name: 'Пепперони',
+                imageUrl: 'https://media.dodostatic.net/image/r:584x584/11EE7D612FC7B7FCA5BE822752BEE1E5.avif',
+                price: 550,
+                items: [{price: 550}],
+                
+              },
+              {
+                id: 2,
+                name: 'Двойной цыпленок',
+                imageUrl: 'https://media.dodostatic.net/image/r:584x584/11EE7D614CBE0530B7234B6D7A6E5F8E.avif',
+                price: 550,
+                items: [{price: 550}],
+                
+              }
+              ]} categoryId={1} />
+              <ProductGroupList title={"Напитки"} items={[{
+                id: 1,
+                name: 'Какао',
+                imageUrl: 'https://media.dodostatic.net/image/r:584x584/11EE79702DC5EA0EBF92E2483DB89B11.avif',
+                price: 550,
+                items: [{price: 550}],
+                
+              },
+              {
+                id: 2,
+                name: 'Апельсиновый сок Rich',
+                imageUrl: 'https://media.dodostatic.net/image/r:584x584/11EE7D61B223E75EB71498BCAA0D4A0D.avif',
+                price: 550,
+                items: [{price: 550}],
+                
+              },
+              {
+                id: 2,
+                name: 'Таежный чай, 10 пакетиков',
+                imageUrl: 'https://media.dodostatic.net/image/r:584x584/11EE797026C5FA9A8298F6EBED364569.avif',
+                price: 550,
+                items: [{price: 550}],
+                
+              }
+              ]} categoryId={2} />
+              
             </div>
           </div>
         </div>
