0ffd1cc8026f472895ec789917fd5fb5dc42f76e
Алексей <savageseed@yandex.ru>
2024-09-18 14:20:42 +0500

    Created product card visual

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,5 @@
-import {
-  Container,
-  Title,
-  TopBar,
-  Filter,
-} from "@/components/shared";
+import { Container, Title, TopBar, Filter } from "@/components/shared";
+import { ProductCard } from "@/components/shared/product-card";
 
 export default function Home() {
   return (
@@ -18,19 +14,22 @@ export default function Home() {
         <div className="flex gap-[60px]">
           {/* Filter */}
           <div className="w-[250px]">
-            <Filter/>
+            <Filter />
           </div>
 
           {/* Product List */}
           <div className="flex-1">
-              <div className="flex flex-col gap-16">
-                Список товаров
-              </div>
+            <div className="flex flex-col gap-16">
+              <ProductCard
+                id={0}
+                name={"Сырная"}
+                price={420}
+                imageUrl="https://media.dodostatic.net/image/r:584x584/11EE7D610D2925109AB2E1C92CC5383C.avif"
+              />
+            </div>
           </div>
         </div>
       </Container>
-      
-      
     </>
   );
 }
